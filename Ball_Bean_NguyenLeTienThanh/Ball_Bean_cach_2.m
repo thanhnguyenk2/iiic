1d1d6173cc3ce5898f46b9f09c4db0c96e7fcb62
NLThanh <87068718+thanhnguyenk2@users.noreply.github.com>
2021-11-07 21:18:12 +0700

    updateee

--- Ball_Bean_NguyenLeTienThanh/Ball_Bean_cach_2.m
+++ Ball_Bean_NguyenLeTienThanh/Ball_Bean_cach_2.m
@@ -1,5 +1,6 @@
 close all
 clc
+load('data_ball_bean.mat');
 %Du lieu huan luyen mang NN
 K = length(out.u); %so mau du lieu
 X = [out.e(2:K)'; out.e(1:K-1)'];
@@ -15,7 +16,7 @@ D = out.u(2:K)';
 % plot(out.u)
 % title('u');
 %Khoi tao mang NN va huan luyen
-N = 10; % so no ron o lop an
+N = 9; % so no ron o lop an
 mynet = newff(X,D,N,{'tansig' 'purelin'});
 mynet = train(mynet,X,D);
 
